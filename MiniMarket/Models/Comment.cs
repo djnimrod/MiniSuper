7377a022c1efb45786832710f0ba1b9626afbadb
danielDev <valcyum.085@gmail.com>
2019-08-18 14:36:16 -0400

    creada tabla comentarios para guardar los comentarios en una seccion de la pagina

---
+++ MiniMarket/Models/Comment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MiniMarket.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Correo Electronico")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Comentario")]
+        [StringLength(120)]
+        [DataType(DataType.MultilineText)]
+        public string Comentario { get; set; }
+
+    }
+}
